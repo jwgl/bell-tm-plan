60ebb9b5f8819e4f49743c5e3f2383d6db60a4ea
todoubaba <todoubaba@gmail.com>
2017-11-23 23:50:47 +0800

    Limit scheme course export by term

--- tm-plan-api/grails-app/controllers/cn/edu/bnuz/bell/planning/SchemeToesController.groovy
+++ tm-plan-api/grails-app/controllers/cn/edu/bnuz/bell/planning/SchemeToesController.groovy
@@ -1,13 +1,23 @@
 package cn.edu.bnuz.bell.planning
 
+import cn.edu.bnuz.bell.master.TermService
+
 class SchemeToesController {
     SchemeDepartmentService schemeDepartmentService
     SchemeToesService schemeToesService
+    TermService termService
 
     def index(String departmentId, Long schemeDepartmentId) {
         def scheme = schemeDepartmentService.getScheme(departmentId, schemeDepartmentId)
         def programCourses = schemeToesService.getProgramCourses(scheme.programId)
-        renderJson([scheme: scheme, programCourses: programCourses])
+        def termId = termService.activeTerm.id
+        def schemeTerm = (termId.intdiv(10) - scheme.grade) * 2 + termId % 10
+
+        renderJson([
+                scheme        : scheme,
+                programCourses: programCourses,
+                schemeTerm    : schemeTerm,
+        ])
     }
 
     def save(String departmentId, Long schemeDepartmentId) {
